5fb86ef60d112aed4c3030101aa34a0b6892cce8
grig.duta <grig.duta@bunnyshell.com>
2023-09-07 13:07:06 +0300

    Added Tags for deployment options

--- register_deploy.py
+++ register_deploy.py
@@ -1,4 +1,5 @@
 from qwak import QwakClient
+from qwak.qwak_client.builds.build import BuildStatus
 
 import subprocess
 import re
@@ -8,6 +9,9 @@ import time
 
 
 SLEEP_BETWEEN_STATUS_QUERY = 10.0
+
+# Initialize the Qwak client
+_qwak_client = QwakClient()
 
 # Define a dictionary to map status codes to their string representations
 _status_code_to_name = {
@@ -46,10 +50,40 @@ def deploy_command():
     if model_id:
         command.extend(["--model-id", model_id])
 
+    # Tags List - will be used later
+    tags_list = os.getenv('TAGS')
+
     # Build ID
     build_id = os.getenv('BUILD_ID')
     if build_id:
         command.extend(["--build-id", build_id])
+        print( "Build ID was specified, ignoring TAGS.\n")
+
+    elif tags_list:
+        tags = tags_list.split(",")
+
+        print (f"The following Tags were specified: {tags} -> Looking for the latest SUCCESSFUL build with these Tags.\n")
+
+        builds = _qwak_client.get_builds_by_tags(model_id=model_id,
+                                        tags=tags)
+        if builds:
+            successful_builds = [build for build in builds if build.build_status == BuildStatus.SUCCESSFUL]
+
+            if successful_builds:
+                successful_builds.sort(key=lambda r: r.created_at)
+
+                command.extend(["--build-id", successful_builds[-1].build_id])
+
+                print (f"Found successful Build with the specified tags - Build ID: {successful_builds[-1].build_id}\n")
+            else: 
+                print ("No successful builds with these tags were found. Exiting...\n")
+                exit(1)
+        else: 
+            print ("No builds with these tags were found. Exiting...\n")
+            exit(1)
+
+    else:
+        print("Neither BUILD_ID nor TAGS provided -> Deploying the latest successful Build.\n")
 
     # Parameter List
     param_list = os.getenv('PARAM_LIST')
@@ -96,9 +130,6 @@ def deploy_command():
 
 def wait_for_deployment(deployment_id: str, timeout: int) -> str:
 
-    # Initialize the Qwak client
-    qwak_client = QwakClient()
-
     # Record the start time to track how long we've been waiting
     start_time = time.time()
     
@@ -111,7 +142,7 @@ def wait_for_deployment(deployment_id: str, timeout: int) -> str:
             time.sleep(SLEEP_BETWEEN_STATUS_QUERY)
 
             # Get the current deployment object from the Qwak client
-            deployment_status_object = qwak_client._get_deployment_management().get_deployment_status(deployment_id)
+            deployment_status_object = _qwak_client._get_deployment_management().get_deployment_status(deployment_id)
 
             verbal_deployment_status = _get_status_name(deployment_status_object.status)
 
